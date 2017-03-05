546b8f00665a416ed5e58b63af2689b4a5904fd9
Remy Da Costa Faro <remy@dacostafaro.com>
2017-03-05 14:50:03 -0500

    Add Carthage support

---
+++ DGElasticPullToRefresh/DGElasticPullToRefresh.h
@@ -0,0 +1,19 @@
+//
+//  DGElasticPullToRefresh.h
+//  DGElasticPullToRefresh
+//
+//  Created by Rémy DA COSTA FARO on 3/5/17.
+//  Copyright © 2017 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGElasticPullToRefresh.
+FOUNDATION_EXPORT double DGElasticPullToRefreshVersionNumber;
+
+//! Project version string for DGElasticPullToRefresh.
+FOUNDATION_EXPORT const unsigned char DGElasticPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGElasticPullToRefresh/PublicHeader.h>
+
+
