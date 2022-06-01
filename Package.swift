273c394be04fad9739b4a6faca1f35980b04042d
Sebastian Nitu <61966540+sebanitu@users.noreply.github.com>
2022-06-01 10:32:23 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,26 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "DGElasticPullToRefresh",
-    platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DGElasticPullToRefresh",
-            targets: ["DGElasticPullToRefresh"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            targets: ["DGElasticPullToRefresh"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DGElasticPullToRefresh",
-            resources: [
-                .process("DGElasticPullToRefreshPreview1.gif"),
-                .process("DGElasticPullToRefreshPreview2.gif"),
-                .copy("settings.plist")
-            ]
-        )
+            name: "DGElasticPullToRefresh"),
     ]
 )
